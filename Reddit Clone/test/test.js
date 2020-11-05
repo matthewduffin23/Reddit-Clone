ddd1d8c5d864960f7ab6b8a2e9ebb6718664a819
matthewduffin23 <65633806+matthewduffin23@users.noreply.github.com>
2020-11-04 23:06:24 -0800

    this is the final project

---
+++ Reddit Clone/test/test.js
@@ -0,0 +1,12 @@
+import { EnzymeAdapter } from 'enzyme';
+import React from 'react';
+import { shallow, mount, render} from 'enzyme';
+import { TestScheduler } from 'jest';
+
+test('Make sure the app is functional', () => {
+    expect(App()).toBe(true);
+});
+
+
+
+export default test.js 
